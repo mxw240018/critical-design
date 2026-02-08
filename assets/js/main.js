3fd2b5a6e3f23bc33bcfef7b0e391e3fb511acde
mxw240018 <mxw240018@utdallas.edu>
2026-02-08 20:31:46 +0000

    format

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,12 @@ let targetElement = document.querySelector("h2");
 console.log(targetElement.outerHTML);
 
 targetElement.addEventListener("mouseover", function () {
-    console.log(this.innerText);
-    this.innerText = "TIME";
-    this.style = "cursor: wait";
+  console.log(this.innerText);
+  this.innerText = "TIME";
+  this.style = "cursor: wait";
 });
 
 targetElement.addEventListener("mouseout", function () {
-    this.innerText = "IKEA";
-    this.style = "cursor: pointer";
-})
+  this.innerText = "IKEA";
+  this.style = "cursor: pointer";
+});
